ebef77a90c0a54cdbcd7db74e3f994f382f65eab
Deathtosmoothie <russs_sl@mail.ru>
2019-05-22 15:48:42 +0600

    third path added nw

--- sbkzrt_portfolio/main.js
+++ sbkzrt_portfolio/main.js
@@ -12,12 +12,36 @@ $(function() {
   // Create the Scene and trigger when visible
   var scene = new ScrollMagic.Scene({
     triggerElement: '#scene',
-    offset: 350 /* offset the trigger 150px below #scene's top */
+    offset: 350 /* offset the trigger offset below #scene's top */
   })
   .setTween(tween)
   .addTo(scrollMagicController);
   
   // Add debug indicators fixed on right side
    scene.addIndicators();
+  
+});
+
+// When the DOM is ready
+$(function() {
+  
+  // Init ScrollMagic Controller
+  var scrollMagicController2 = new ScrollMagic.Controller(),
+  hippo = document.getElementById("hippo");
+  // Create Animation for 0.5s
+  var tween2 = TweenLite.to(hippo, 1, {morphSVG:"#third"})
+  
+
+  
+  // Create the Scene and trigger when visible
+  var scene2 = new ScrollMagic.Scene({
+    triggerElement: '#scene2',
+    offset: 2000 /* offset the trigger offset below #scene's top */
+  })
+  .setTween(tween2)
+  .addTo(scrollMagicController2);
+  
+  // Add debug indicators fixed on right side
+   scene2.addIndicators();
   
 });
