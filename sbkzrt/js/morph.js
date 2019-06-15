79823d50182560fa7a5db605b199e68600015c3c
Deathtosmoothie <russs_sl@mail.ru>
2019-06-15 14:40:33 +0600

    right ear connected

--- sbkzrt/js/morph.js
+++ sbkzrt/js/morph.js
@@ -8,7 +8,7 @@ $(window).on('load', function(){
 	second_scene = []
 	
 	//LEFT EAR - LEFT FINGER
-	for(i=0;i<11;i++) {
+	for(i=0;i<12;i++) {
 		first_scene.push($('#svg_1').contents().find('#ear_l' + [i])[0]);
 	}
 	
@@ -18,45 +18,55 @@ $(window).on('load', function(){
 	}
 	
 	
-	//NOSE BLACK - NOSE WHITE
+	//ADDITIONAL PATHS
 	var $nose_black = $('#svg_1').contents().find('#nose_black')[0];
 	var $nose_white = $('#svg_2').contents().find('#nose_white')[0];
 	
 	var $fill_1 = $('#svg_1').contents().find('#fill_1')[0];
 	var $fill_2 = $('#svg_2').contents().find('#fill_2')[0];
 	
-	var $ear_blur = $('#svg_1').contents().find('#ear_blur')[0];
+	var $ear_l12 = $('#svg_1').contents().find('#ear_l12')[0];
+	var $finger_l10 = $('#svg_2').contents().find('#finger_l10')[0];
+	
 	var $white_blur = $('#svg_2').contents().find('#white_blur')[0];
 	var $pre_white_blur = $('#svg_1').contents().find('#pre_white_blur')[0];
 	
 	
-	//ADDITIONAL PATHS
-	first_scene.unshift($nose_black,$pre_white_blur,$fill_1);
-	second_scene.unshift($nose_white,$white_blur,$fill_2);
+	first_scene.unshift($nose_black,$pre_white_blur,$fill_1,$ear_l12);
+	second_scene.unshift($nose_white,$white_blur,$fill_2,$finger_l10);
 	
-	console.log(first_scene.length,second_scene.length)
-
-
+	
+	
 	
 	//RIGHT EAR - RIGHT FINGER
 	for(i=1;i<8;i++) {
 		first_scene.push($('#svg_1').contents().find('#ear_r' + [i])[0]);
 	}
 	
-
+	for(i=0;i<9;i++) {
+		second_scene.push($('#svg_2').contents().find('#finger_r' + [i])[0]);
+	}
 	
-	for(i=0; i < 12;i++) {
+console.log(first_scene.length,second_scene.length)
+	console.log(second_scene[12])
+	
+	for(i=0; i < 23;i++) {
 		
-		var morph_obj = {morphSVG:second_scene[i]};
+		morph_obj = {morphSVG:second_scene[i]};
 		
 		if(i<2) {
 			morph_obj = {morphSVG:second_scene[i],fill:"#FFF"};
 		}
 		
+		
+		if(i>12 && i<14) {
+			morph_obj = {morphSVG:second_scene[11]};
+		}
+		
 		var tween = new TimelineMax()
     	.to(first_scene[i], 1, morph_obj);
-		console.log(i);
-	
+//		console.log(i);
+//	
   // Create the Scene and trigger when visible
   var scene = new ScrollMagic.Scene({
     triggerElement: 'main',
